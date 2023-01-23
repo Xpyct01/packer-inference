252f11400a73692228658cbaf9a8f21106fae990
Xpyct228 <kopitko94@gmail.com>
2023-01-23 09:17:55 +0200

    added main.py, model.gz, requirements.txt

---
+++ main.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+import joblib
+import json
+
+
+MODEL_PATH = "model.gz"
+app = Flask(__name__)
+
+
+def get_predictions(input_data):
+    model = joblib.load(MODEL_PATH)
+    result = model.predict([input_data])[0]
+    return {"result": result}
+
+
+@app.route("/predict", methods=['POST'])
+def predict():
+    request_data = request.get_json()
+    input_data = request_data['input_data']
+    return get_predictions(input_data)
+
+
+app.run()
